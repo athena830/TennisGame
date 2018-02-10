a2511306cc3c802454bcdff1af5a553286bf7fc0
athena830 <athena830@gmail.com>
2018-02-10 17:07:53 +0800

    first commit

---
+++ TennisGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TennisGameTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TennisGameTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37219136-0c42-401a-b028-b27f287bd0c6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TennisGame/TennisGameTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGame
+{
+    [TestClass]
+    public class TennisGameTest
+    {
+        TennisGame tennisGame = new TennisGame();
+
+        [TestMethod]
+        public void Love_All()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            GivenFirstPlayerScoreTimes(1);
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        [TestMethod]
+        public void Thirty_Love()
+        {
+            GivenFirstPlayerScoreTimes(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        [TestMethod]
+        public void Love_Fifteen()
+        {
+            GivenSecondPlayerScore(1);
+            ScoreShouldBe("Love Fifteen");
+        }
+
+        [TestMethod]
+        public void Love_Thirty()
+        {
+            GivenSecondPlayerScore(2);
+            ScoreShouldBe("Love Thirty");
+        }
+        private void GivenFirstPlayerScoreTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                tennisGame.FirstPlayerScore();
+            }
+        }
+        private void GivenSecondPlayerScore(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                tennisGame.SecondPlayerScore();
+            }
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, tennisGame.Score());
+        }
+    }
+
+    public class TennisGame
+    {
+        private int _firstPlayerScoreTimes;
+        private int _secondPlayerScoreTimes;
+
+        private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            {0, "Love"},
+            {1, "Fifteen"},
+            {2, "Thirty"},
+            {3, "Forty"},
+        };
+
+        public string Score()
+        {
+            if (_firstPlayerScoreTimes>0 || _secondPlayerScoreTimes>0)
+            {
+                return _scoreLookup[_firstPlayerScoreTimes] + " " + _scoreLookup[_secondPlayerScoreTimes];
+            }
+            if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes > 0)
+            {
+                return "Love " + _scoreLookup[_secondPlayerScoreTimes];
+            }
+            if (_secondPlayerScoreTimes == 0 && _firstPlayerScoreTimes > 0)
+            {
+                return _scoreLookup[_firstPlayerScoreTimes] + " Love";
+            }
+            return "Love All";
+        }
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScoreTimes++;
+        }
+        public void SecondPlayerScore()
+        {
+            _secondPlayerScoreTimes++;
+        }
+    }
+}
